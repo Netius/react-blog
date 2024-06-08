f11f334e8aec64bd181523d84a237519131bf615
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2024-06-08 14:36:41 +0200

    Started with react blog and pages, routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage';
+import ArticleListPage from './pages/ArticleListPage';
+import ArticlePage from './pages/ArticlePage';
+import AboutPage from './pages/AboutPage';
+
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>React blog</h1>
+        <div id='page-body'>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/article" element={<ArticlePage />} />
+            <Route path="/articles/:articleId" element={<ArticleListPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ArticleListPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArticleListPage = () => {
+  return (
+    <h1>ArticleListPage</h1>
+  )
+}
+
+export default ArticleListPage
