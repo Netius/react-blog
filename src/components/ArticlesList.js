a1a86bf4c024a399ff3b8fa55f847b5067c76f06
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2024-06-09 12:00:36 +0200

    Displaying list as a component

---
+++ src/components/ArticlesList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ArticlesList = ({articles}) => {
+	return (
+		<>
+			{articles.map((article, index) => (
+				<Link className='article-list-item' to={`/articles/${article.name}`} key={index}>
+					<h2>{article.title}</h2>
+					<p>{article.content[0].substring(0, 150)}...</p>
+				</Link>
+			))}
+		</>
+	)
+}
+
+export default ArticlesList
--- src/pages/ArticleListPage.js
+++ src/pages/ArticleListPage.js
@@ -1,17 +1,12 @@
 import React from 'react';
+import ArticlesList from '../components/ArticlesList';
 import articles from './article-content';
-import { Link } from 'react-router-dom';
 
 const ArticleListPage = () => {
 	return (
 		<>
 			<h1>Articles</h1>
-			{articles.map((article, index) => (
-				<Link className='article-list-item' to={`/articles/${article.name}`} key={index}>
-					<h2>{article.title}</h2>
-					<p>{article.content[0].substring(0, 150)}...</p>
-				</Link>
-			))}		
+			<ArticlesList articles={articles} />
 		</>
 	)
 }
