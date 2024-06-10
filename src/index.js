d880934e56d8583c7d8f51ccdb999022c306c795
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2024-06-10 21:13:23 +0200

    Added Firebase and code to prj

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBh9JYxJtKNErQ3ssTKP_YOT-PhK4RHDnU",
+  authDomain: "react-blog-46b81.firebaseapp.com",
+  projectId: "react-blog-46b81",
+  storageBucket: "react-blog-46b81.appspot.com",
+  messagingSenderId: "901489943018",
+  appId: "1:901489943018:web:7bd3d64a2682a4a846d293"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
