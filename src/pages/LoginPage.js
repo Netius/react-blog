51b688211b6da3c5939409ec3bcbf0d02dd2dae4
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2024-06-10 21:30:18 +0200

    Log in user to FireBase Auth

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const logIn = async () => {
+    try {
+      await signInWithEmailAndPassword(getAuth(), email, password);
+      navigate("/articles");
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
   return (
-    <h1>LoginPage</h1>
+    <>
+      <h1>Log in</h1>
+      {error && <p className='error'>{error}</p>}
+      <input placeholder='Your email' value={email} onChange={e => setEmail(e.target.value)} />
+      <input type='password' placeholder='Your password' value={password} onChange={e => setPassword(e.target.value)} />
+      <button type='button' onClick={logIn}>Log in</button>
+      <Link to={"/create-account"}>Create a account here</Link>
+    </>
   )
 }
 
