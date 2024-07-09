118340e287c8e9a8fb5ca6e706c5c1c328aa544b
Fabio <fabio@axicon.no>
2024-07-09 09:49:34 +0200

    Add active style to nav

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import  useUser  from './hooks/useUser';
 import { getAuth, signOut } from 'firebase/auth';
 
@@ -9,9 +9,9 @@ const NavigationBar = () => {
   return (
     <nav>
       <ul>
-        <li><Link to={"/"}>Home</Link></li>
-        <li><Link to={"/about"}>About</Link></li>
-        <li><Link to={"/articles"}>Articles</Link></li>
+        <li><NavLink to={"/"}>Home</NavLink></li>
+        <li><NavLink to={"/articles"}>Articles</NavLink></li>
+        <li><NavLink to={"/about"}>About</NavLink></li>
       </ul>
       <div className='nav-right'>
         {user?.email}
